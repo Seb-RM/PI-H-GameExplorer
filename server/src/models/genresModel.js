001f6aa3b39e02d38638b97787dbf00482328783
Seb-RM <srmmoto@gmail.com>
2023-11-28 11:45:38 -0300

    creación de modelos

---
+++ server/src/models/genresModel.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../DB_connection.js";
+
+const Genre = sequelize.define("Genre", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.ENUM(
+        "Action",
+        "Indie",
+        "Adventure",
+        "RPG",
+        "Strategy",
+        "Shooter",
+        "Casual",
+        "Simulation",
+        "Puzzle",
+        "Arcade",
+        "Platformer",
+        "Massively Multiplayer",
+        "Racing",
+        "Sports",
+        "Fighting",
+        "Family",
+        "Board Games",
+        "Educational",
+        "Card",
+        "Other"
+        ),
+        allowNull: false,
+        defaultValue: "Other",
+    },
+});
+
+export default Genre;
