7aeb52f4178fa1fe7681b1f966fb999248d5eb61
Seb-RM <srmmoto@gmail.com>
2023-11-30 23:15:28 -0300

    avances en la landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/LandingPage.css"; // Asegúrate de tener un archivo CSS para los estilos
 
 const LandingPage = () => {
     return (
         <section className="landing-page">
-        <div className="marquee">
-            <span>Bienvenidos a Game Explorer</span>
-        </div>
-        <p>Haz clic para continuar</p>
-        <button>Continuar</button>
+            <div className="marquee">
+                <span>Bienvenidos a Game Explorer</span>
+            </div>
+            <p className="message">Haz clic para continuar</p>
+            <Link>
+                <button className="button">
+                    <span></span>Continuar
+                </button>
+            </Link>
         </section>
     );
 };
